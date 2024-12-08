98a8c49cd02b7d7d9b404fec5c9b1e30da28f087
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-08 19:51:45 +0530

    Update final_arm_pca.ino

--- final_arm_pca.ino
+++ final_arm_pca.ino
@@ -5,58 +5,78 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVO_MIN 150
 #define SERVO_MAX 600
-#define SMOOTHNESS 5  // Reduce to make smoother and less twitchy
-#define THRESHOLD 10   // Threshold for potentiometer changes
-#define DEADZONE 5     // Deadzone to avoid tiny changes
+#define SMOOTHNESS 5
+#define THRESHOLD 10
+#define DEADZONE 5
 
 const int potPins[] = {A1, A2, A3, A6, A7};
-int potValues[5] = {0, 0, 0, 0, 0};
-int lastPotValues[5] = {0, 0, 0, 0, 0};
-
+int potValues[5];
+int lastPotValues[5];
 const int servoChannels[] = {0, 1, 2, 3, 4};
 
 void setup() {
   pwm.begin();
-  pwm.setPWMFreq(50);  // Frequency for servos
+  pwm.setPWMFreq(50);
+
   for (int i = 0; i < 5; i++) {
     potValues[i] = analogRead(potPins[i]);
+    lastPotValues[i] = potValues[i];
+
+    int mappedValue;
+    if (i == 4) { // A7 (360 degrees)
+      mappedValue = map(potValues[i], 0, 1023, 0, 360);
+    } else { // Other servos (180 degrees)
+      mappedValue = map(potValues[i], 0, 1023, 0, 180);
+    }
+    int initialPulse = map(mappedValue, i == 4 ? 0 : 0, i == 4 ? 360 : 180, SERVO_MIN, SERVO_MAX);
+    pwm.setPWM(servoChannels[i], 0, initialPulse);
   }
 }
 
 void loop() {
-  int totalAngle = 0;
+  int potA1 = analogRead(potPins[0]);
+  int potA2 = analogRead(potPins[1]);
+  int potA3 = analogRead(potPins[2]);
+  int potA6 = analogRead(potPins[3]);
+  int potA7 = analogRead(potPins[4]);
 
-  for (int i = 0; i < 5; i++) {
-    int potValue = analogRead(potPins[i]);
-
-    // Apply deadzone to reduce small changes and avoid jitter
-    if (abs(potValue - lastPotValues[i]) > THRESHOLD) {
-      lastPotValues[i] = potValue;
-      if (i == 4) {  // A7 (base), 360-degree range
-        potValues[i] = map(potValue, 0, 1023, 0, 360);
-      } else {  // Other servos, 180-degree range
-        potValues[i] = map(potValue, 0, 1023, 0, 180);
-      }
-    }
-
-    totalAngle += potValues[i];
+  if (abs(potA1 - lastPotValues[0]) > THRESHOLD) {
+    lastPotValues[0] = potA1;
+    potValues[0] = map(potA1, 0, 1023, 0, 180);
+  }
+  if (abs(potA2 - lastPotValues[1]) > THRESHOLD) {
+    lastPotValues[1] = potA2;
+    potValues[1] = map(potA2, 0, 1023, 0, 180);
+  }
+  if (abs(potA3 - lastPotValues[2]) > THRESHOLD) {
+    lastPotValues[2] = potA3;
+    potValues[2] = map(potA3, 0, 1023, 0, 180);
+  }
+  if (abs(potA6 - lastPotValues[3]) > THRESHOLD) {
+    lastPotValues[3] = potA6;
+    potValues[3] = map(potA6, 0, 1023, 0, 180);
+  }
+  if (abs(potA7 - lastPotValues[4]) > THRESHOLD) {
+    lastPotValues[4] = potA7;
+    potValues[4] = map(potA7, 0, 1023, 0, 360);
   }
 
-  // Update servos with smoothed values
-  for (int i = 0; i < 5; i++) {
-    int currentAngle = potValues[i];
-    int pulseWidth;
+  int pulseWidthA1 = map(potValues[0], 0, 180, SERVO_MIN, SERVO_MAX);
+  moveServoSmoothly(servoChannels[0], pulseWidthA1);
 
-    if (i == 4) {  // A7 (base), 360 degrees
-      pulseWidth = map(currentAngle, 0, 360, SERVO_MIN, SERVO_MAX);
-    } else {  // Other servos, 180 degrees
-      pulseWidth = map(currentAngle, 0, 180, SERVO_MIN, SERVO_MAX);
-    }
+  int pulseWidthA2 = map(potValues[1], 0, 180, SERVO_MIN, SERVO_MAX);
+  moveServoSmoothly(servoChannels[1], pulseWidthA2);
 
-    moveServoSmoothly(servoChannels[i], pulseWidth);
-  }
+  int pulseWidthA3 = map(potValues[2], 0, 180, SERVO_MIN, SERVO_MAX);
+  moveServoSmoothly(servoChannels[2], pulseWidthA3);
 
-  delay(20);  // Shorter delay for more responsiveness
+  int pulseWidthA6 = map(potValues[3], 0, 180, SERVO_MIN, SERVO_MAX);
+  moveServoSmoothly(servoChannels[3], pulseWidthA6);
+
+  int pulseWidthA7 = map(potValues[4], 0, 360, SERVO_MIN, SERVO_MAX);
+  moveServoSmoothly(servoChannels[4], pulseWidthA7);
+
+  delay(20);
 }
 
 void moveServoSmoothly(int channel, int targetPulse) {
