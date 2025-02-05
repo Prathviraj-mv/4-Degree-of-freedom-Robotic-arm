91fe8b2f617a8f38b8e48386b2f3f89b515ce564
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2025-02-05 23:45:15 +0530

    Update final_arm_pca.ino

--- final_arm_pca.ino
+++ final_arm_pca.ino
@@ -6,21 +6,26 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_MIN 150
 #define SERVO_MAX 600
 #define DEADZONE 10
+#define STEP_ANGLE 5
 
 const int potPins[] = {A1, A2, A3, A6, A7};
 const int servoChannels[] = {0, 1, 2, 3, 4};
 int lastPotValues[5];
+int targetAngles[5] = {0, 0, 0, 0, 0};
+int currentAngles[5] = {0, 0, 0, 0, 0};
 
 void setup() {
   pwm.begin();
   pwm.setPWMFreq(50);
 
   for (int i = 0; i < 5; i++) {
-    int potValue = analogRead(potPins[i]);
-    lastPotValues[i] = potValue;
-
-    int mappedAngle = map(potValue, 0, 1023, 0, (i == 4 ? 360 : 180));
-    int initialPulse = map(mappedAngle, 0, (i == 4 ? 360 : 180), SERVO_MIN, SERVO_MAX);
+    lastPotValues[i] = analogRead(potPins[i]);
+    
+    targetAngles[i] = map(lastPotValues[i], 0, 1023, 0, (i == 4 ? 360 : 180));
+    targetAngles[i] -= (targetAngles[i] % STEP_ANGLE);
+    currentAngles[i] = targetAngles[i];
+    
+    int initialPulse = map(currentAngles[i], 0, (i == 4 ? 360 : 180), SERVO_MIN, SERVO_MAX);
     pwm.setPWM(servoChannels[i], 0, initialPulse);
   }
 }
@@ -32,10 +37,18 @@ void loop() {
     if (abs(potValue - lastPotValues[i]) > DEADZONE) {
       lastPotValues[i] = potValue;
 
-      int mappedAngle = map(potValue, 0, 1023, 0, (i == 4 ? 360 : 180));
-      int pulseWidth = map(mappedAngle, 0, (i == 4 ? 360 : 180), SERVO_MIN, SERVO_MAX);
-      pwm.setPWM(servoChannels[i], 0, pulseWidth);
+      targetAngles[i] = map(potValue, 0, 1023, 0, (i == 4 ? 360 : 180));
+      targetAngles[i] -= (targetAngles[i] % STEP_ANGLE);
     }
+    
+    if (currentAngles[i] < targetAngles[i]) {
+      currentAngles[i] += STEP_ANGLE;
+    } else if (currentAngles[i] > targetAngles[i]) {
+      currentAngles[i] -= STEP_ANGLE;
+    }
+    
+    int pulseWidth = map(currentAngles[i], 0, (i == 4 ? 360 : 180), SERVO_MIN, SERVO_MAX);
+    pwm.setPWM(servoChannels[i], 0, pulseWidth);
   }
-  delay(20);
+  delay(50); // Slow movement
 }
